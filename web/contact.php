b43bb58bd96e06770ea353c21d94b28fbb1b2453
cdthomp1 <tho17015@byui.edu>
2020-01-18 14:56:47 -0700

    test

--- web/contact.php
+++ web/contact.php
@@ -16,14 +16,26 @@
     <div id="content">
         <header>
             <a href="index.php"><img src="Dev.png" /></a>
-            <h1>Cameron Thompson</h1>
-            <h2>Student | Student Software Developer | Thinker</h2>
-        </header>
-        <article>
-            <section>
-                <p>Hey I haven't been made yet!</p>
-            </section>
-        </article>
+            <h1>Getting in touch is easy!</h1>
+                    <p><strong>Interested in working together?</strong> Fill out the form below and we can get started on creating something amazing together!</p>
+                </div>
+            </div>
+            <form action="" method="post">
+                <div class="row">
+                    <div class="six columns">
+                        <label for="email">Your email</label>
+                        <input class="u-full-width" name="email" type="email" placeholder="Your email..." id="email">
+                    </div>
+                    <div class="six columns">
+                        <label for="name">Your Name</label>
+                        <input class="u-full-width" name="name" type="text" placeholder="Your name..." id="name">
+                    </div>
+                </div>
+                <label for="message">Message</label>
+                <textarea class="u-full-width" name="message" placeholder="Hi Joseph..." id="message"></textarea>
+                <button type="submit" name="submit" class="button">Submit</div>
+            </form>
+        </div>
         <?php echo "<footer><img id=\"footerImg\" src=\"Dev.png\" /><p>Copyright &copy; " . date("Y") . " Cameron Thompson</p><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.php\">Contact</a></li></ul></footer>";?>
 
     </div>
