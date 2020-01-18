f920e721f80a8c5a123d669cec43b2b9765476cc
cdthomp1 <tho17015@byui.edu>
2020-01-18 14:46:19 -0700

    chnage

--- web/index.php
+++ web/index.php
@@ -35,7 +35,7 @@
                 </div>
             </section>
         </article>
-        <?php echo "<footer><div id=\"footerImage\"><img id=\"footerImg\" src=\"Dev.png\" />Copyright &copy; " . date("Y") . " Cameron Thompson</div><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.html\">Contact</a></li></ul></footer>";?>
+        <?php echo "<footer><img id=\"footerImg\" src=\"Dev.png\" /><p>Copyright &copy; " . date("Y") . " Cameron Thompson</p><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.html\">Contact</a></li></ul></footer>";?>
     </div>
     <script src="particles.js"></script>
 
