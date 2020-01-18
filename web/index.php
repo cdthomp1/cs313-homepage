9bcefb596233f737ee4167ca09353b7ee5fe049d
cdthomp1 <tho17015@byui.edu>
2020-01-18 14:38:46 -0700

    update footer

--- web/index.php
+++ web/index.php
@@ -35,16 +35,7 @@
                 </div>
             </section>
         </article>
-        <footer>
-            <img id="footerImg" src="Dev.png" />
-            <ul class="footerLinks">
-                <li><a href="https://www.linkedin.com/in/cameron-thompson96/" target="_blank"
-                        rel="noopener noreferrer">Linkedin</a></li>
-                <li><a href="https://github.com/cdthomp1" target="_blank" rel="noopener noreferrer">Github</a></li>
-
-                <li><a href="/contact.html">Contact</a></li>
-            </ul>
-        </footer>
+        <?php echo "<footer><img id=\"footerImg\" src=\"Dev.png\" /><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.html\">Contact</a></li></ul></footer>";?>
     </div>
     <script src="particles.js"></script>
 
