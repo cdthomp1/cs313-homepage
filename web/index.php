9097ffa03016a1b39761df5b066993552c862cf7
cdthomp1 <tho17015@byui.edu>
2020-01-18 14:52:17 -0700

    change link

--- web/index.php
+++ web/index.php
@@ -30,7 +30,7 @@
                 <a class="button" href="assignments.php">Assignemnts</a>
             </section>
         </article>
-        <?php echo "<footer><img id=\"footerImg\" src=\"Dev.png\" /><p>Copyright &copy; " . date("Y") . " Cameron Thompson</p><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.html\">Contact</a></li></ul></footer>";?>
+        <?php echo "<footer><img id=\"footerImg\" src=\"Dev.png\" /><p>Copyright &copy; " . date("Y") . " Cameron Thompson</p><ul class=\"footerLinks\"><li><a href=\"https://www.linkedin.com/in/cameron-thompson96/\" target=\"_blank\" rel=\"noopener noreferrer\">Linkedin</a></li><li><a href=\"https://github.com/cdthomp1\" target=\"_blank\" rel=\"noopener noreferrer\">Github</a></li><li><a href=\"/contact.php\">Contact</a></li></ul></footer>";?>
     </div>
     <script src="particles.js"></script>
 
